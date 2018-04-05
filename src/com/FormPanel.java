fb4f578dea0e1bf825a56dd3d28033476460800b
Stephenshoemaker99 <stephenshoemaker99@gmail.com>
2018-04-04 22:43:36 -0700

    Im Committeting thid

--- src/com/FormPanel.java
+++ src/com/FormPanel.java
@@ -16,7 +16,7 @@ public class FormPanel extends JPanel {
         setPreferredSize(dim);
 
         nameLabel = new JLabel("Name: ");
-        occupationLabel = new JLabel("Occupation");
+        occupationLabel = new JLabel("Occupation: ");
         nameField = new JTextField(10);
         occupationField = new JTextField( 10);
 
@@ -28,27 +28,43 @@ public class FormPanel extends JPanel {
         setLayout(new GridBagLayout());
 
         GridBagConstraints gc = new GridBagConstraints();
-
+        gc.weightx = 1;
+        gc.weighty = 0.1;
+        //////////// First_Row////////
         gc.gridx = 0;
         gc.gridy = 0;
-        gc.weightx = 1;
-        gc.weighty = 1;
-        gc.fill = GridBagConstraints.NONE;
 
+        gc.fill = GridBagConstraints.NONE;
+        gc.anchor = GridBagConstraints.LINE_END;
+        gc.insets = new Insets(0,0,0,5);
         add(nameLabel, gc);
 
-        gc.gridx = 2;
+        gc.gridx = 1;
         gc.gridy = 0;
-
+        gc.anchor = GridBagConstraints.LINE_START;
+        gc.insets = new Insets(0,0,0,0);
         add(nameField, gc);
+
+        //// Second Row////
+
+        gc.weighty = .1;
         gc.gridx = 0;
         gc.gridy = 1;
+        gc.anchor = GridBagConstraints.LINE_END;
+        gc.insets = new Insets(0,0,0,5);
         add(occupationLabel, gc);
-        gc.gridx = 2;
+        gc.gridx = 1;
         gc.gridy = 1;
+        gc.anchor = GridBagConstraints.LINE_START;
+        gc.insets = new Insets(0,0,0,0);
         add(occupationField, gc);
+
+
+        ////////Third Row/////
+        gc.weighty = 3;
         gc.gridy = 2;
         gc.gridx = 1;
+        gc.anchor = GridBagConstraints.FIRST_LINE_START;
         add(okBtn, gc);
 
 
